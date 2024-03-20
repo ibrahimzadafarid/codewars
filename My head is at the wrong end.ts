e8b6c28080f6da32bc5e1768c6307968c50d23a3
Farid Ibrahimzada <ibrahimzada.farid@gmail.com>
2024-03-20 09:23:43 +0400

    Task_16: My head is at the wrong end

---
+++ My head is at the wrong end.ts
@@ -0,0 +1,7 @@
+// ! My head is at the wrong end! https://www.codewars.com/kata/56f699cd9400f5b7d8000b55
+// ! 8KYU
+
+export function fixTheMeerkat(arr: string[]): string[] {
+    return arr.reverse();
+  }
+  
