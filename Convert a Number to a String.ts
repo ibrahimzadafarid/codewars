5921a36e24e5aee22b58f392725daef1335e954e
Farid Ibrahimzada <ibrahimzada.farid@gmail.com>
2024-03-20 09:22:00 +0400

    Task_09: Convert a Number to a String

---
+++ Convert a Number to a String.ts
@@ -0,0 +1,6 @@
+// ! Convert a Number to a String! https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+// ! 8KYU
+
+export function numberToString(num: number): string {
+    return num.toString()
+  }
