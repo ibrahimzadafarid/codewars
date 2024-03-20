9e40492f4ecd16dcd837ef910a1b6d52be2dfb69
Farid Ibrahimzada <ibrahimzada.farid@gmail.com>
2024-03-20 09:21:27 +0400

    Task_07: Lost Without a Map

---
+++ Beginner - Lost Without a Map.ts
@@ -0,0 +1,6 @@
+// ! Beginner - Lost Without a Map https://www.codewars.com/kata/57f781872e3d8ca2a000007e
+// ! 8KYU
+
+export function maps(x: number[]): number[]{
+    return x.map(num => num*2 );
+  }
