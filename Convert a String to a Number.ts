3bcbb17d516859f6f0aa68fbf3b3fe0ab6b00cfc
Farid Ibrahimzada <ibrahimzada.farid@gmail.com>
2024-03-20 09:22:15 +0400

    Task_10: Convert a String to a Number

---
+++ Convert a String to a Number.ts
@@ -0,0 +1,7 @@
+// ! Convert a String to a Number! https://www.codewars.com/kata/544675c6f971f7399a000e79
+// ! 8KYU
+
+export function stringToNumber(str: string): number {
+    
+    return +str
+  }
