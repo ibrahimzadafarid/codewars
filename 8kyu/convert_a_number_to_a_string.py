c53acfc1db6d2a59616856cf8a13b3dbd6234273
ibrahimzadafarid <ibrahimzada.farid@gmail.com>
2026-02-17 10:48:43 +0400

    Add 2 Codewars solutions (8 kyu)

---
+++ 8kyu/convert_a_number_to_a_string.py
@@ -0,0 +1,13 @@
+"""
+Language: Python
+Kata: Convert a Number to a String!
+Kyu: 8
+Description: We need a function that can transform a number (integer) into a string. What ways of achieving this do you know?
+URL: https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+"""
+
+# Solution
+
+
+def number_to_string(num):
+    return str(num)
---
+++ 8kyu/convert_boolean_values_to_strings_yes_or_no.py
@@ -0,0 +1,13 @@
+"""
+Language: Python
+Kata: Convert boolean values to strings 'Yes' or 'No'.
+Kyu: 8
+Description: Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+URL: https://www.codewars.com/kata/53369039d7ab3ac506000467
+"""
+
+# Solution
+
+
+def bool_to_word(boolean):
+    return "Yes" if boolean else "No"
