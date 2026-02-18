8844c75e4c1a844b4cde5a88a6454ee35d5c1141
ibrahimzadafarid <ibrahimzada.farid@gmail.com>
2026-02-18 15:31:13 +0400

    add Codewars solution grasshopper summation

---
+++ 8kyu/grasshopper_summation.py
@@ -0,0 +1,13 @@
+"""
+Language: Python
+Kata: Grasshopper - Summation
+Kyu: 8
+Description: Write a program that finds the summation of every number from 1 to num (both inclusive). The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
+URL: https://www.codewars.com/kata/55d24f55d7dd296eb9000030
+"""
+
+# Solution
+
+
+def summation(num):
+    return sum(range(1, num + 1))
