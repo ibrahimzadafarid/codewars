c5236df5d3f1f0b15a255e9f2fa07f8012eb175c
ibrahimzadafarid <ibrahimzada.farid@gmail.com>
2026-02-18 10:14:16 +0400

    add Codewars solution (8kyu, opposite number)

---
+++ 8kyu/opposite_number.py
@@ -0,0 +1,12 @@
+"""
+Language: Python
+Kata: Oposite number
+Kyu: 8
+Description: Very simple, given a number, find its opposite (additive inverse).
+URL: https://www.codewars.com/kata/56dec885c54a926dcd001095
+"""
+
+
+# Solution
+def opposite(number):
+    return -number
