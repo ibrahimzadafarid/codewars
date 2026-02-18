b6c67a325540446e65d90c9f32bae5572c271c05
ibrahimzadafarid <ibrahimzada.farid@gmail.com>
2026-02-18 12:46:29 +0400

    add Codewars solution (8kyu, string repeat)

---
+++ 8kyu/string_repeat.py
@@ -0,0 +1,12 @@
+"""
+Language: Python
+Kata: String repeat
+Kyu: 8
+Description: Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+URL: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+"""
+
+
+# Solution
+def repeat_str(repeat, string):
+    return repeat * string
