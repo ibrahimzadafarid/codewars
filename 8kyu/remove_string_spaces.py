5b276fa8048fa4b0244c00b5a212da9793af5dd7
ibrahimzadafarid <ibrahimzada.farid@gmail.com>
2026-02-18 21:45:30 +0400

    add Codewars solution (8kyu, remove string spaces)

---
+++ 8kyu/remove_string_spaces.py
@@ -0,0 +1,13 @@
+"""
+Language: Python
+Kata: Remove String Spaces
+Kyu: 8
+Description: Write a function that removes the spaces from the string, then return the resultant string.
+URL: https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+"""
+
+# Solution
+
+
+def no_space(x):
+    return x.replace(" ", "")
