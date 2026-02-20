69fb026a1291414717cc368d2eac8ee759fc3227
ibrahimzadafarid <ibrahimzada.farid@gmail.com>
2026-02-20 08:16:04 +0400

    add Codewars solution (8kyu, convert a string to a number)

---
+++ 8kyu/convert_a_string_to_a_number.py
@@ -0,0 +1,14 @@
+"""
+Language: Python
+Kata: Convert a String to a Number!
+Kyu: 8
+Description: We need a function that can transform a string into a number. What ways of achieving this do you know?
+URL: https://www.codewars.com/kata/544675c6f971f7399a000e79
+"""
+
+
+# Solution
+
+
+def string_to_number(s):
+    return int(s)
